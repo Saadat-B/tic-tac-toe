8a12a766b169996f97805bac61cc67f4c37ba6e6
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-01-21 09:15:29 +0530

    Added eval function to determine the win/loss/draw

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 
     cacheDom: function () {
       this.grid = document.querySelectorAll(".grid");
+      this.result = document.querySelector("#result");
       // this.grid1 = document.querySelector("#grid-1");
       // this.grid2 = document.querySelector("#grid-2");
       // this.grid3 = document.querySelector("#grid-3");
@@ -36,13 +37,56 @@
           this.innerText = game.input;
           let index = this.dataset.pos;
           game.gameBoard[index] = game.input;
+          game.eval();
         } else {
           game.input = "O";
           game.state = true;
           this.innerText = game.input;
           let index = this.dataset.pos;
           game.gameBoard[index] = game.input;
+          game.eval();
         }
+      }
+    },
+    eval: function () {
+      if (
+        (game.gameBoard[0] === "X" &&
+          game.gameBoard[1] === "X" &&
+          game.gameBoard[2] === "X") ||
+        (game.gameBoard[3] === "X" &&
+          game.gameBoard[4] === "X" &&
+          game.gameBoard[5] === "X") ||
+        (game.gameBoard[6] === "X" &&
+          game.gameBoard[7] === "X" &&
+          game.gameBoard[8] === "X") ||
+        (game.gameBoard[0] === "X" &&
+          game.gameBoard[4] === "X" &&
+          game.gameBoard[8] === "X") ||
+        (game.gameBoard[2] === "X" &&
+          game.gameBoard[4] === "X" &&
+          game.gameBoard[6] === "X")
+      ) {
+        game.result.innerText = "X WINS!!!!!";
+      } else if (
+        (game.gameBoard[0] === "O" &&
+          game.gameBoard[1] === "O" &&
+          game.gameBoard[2] === "O") ||
+        (game.gameBoard[3] === "O" &&
+          game.gameBoard[4] === "O" &&
+          game.gameBoard[5] === "O") ||
+        (game.gameBoard[6] === "O" &&
+          game.gameBoard[7] === "O" &&
+          game.gameBoard[8] === "O") ||
+        (game.gameBoard[0] === "O" &&
+          game.gameBoard[4] === "O" &&
+          game.gameBoard[8] === "O") ||
+        (game.gameBoard[2] === "O" &&
+          game.gameBoard[4] === "O" &&
+          game.gameBoard[6] === "O")
+      ) {
+        game.result.innerText = "O WINS!!!!!";
+      } else if (!game.gameBoard.includes(undefined)) {
+        game.result.innerText = "DRAW";
       }
     },
   };
